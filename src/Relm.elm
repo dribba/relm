3eea466cb5901ad23250fe1e6cbb9423ba389bdd
David Ribba <dribba@gmail.com>
2015-12-22 16:27:48 -0500

    The moving square :D

--- src/Relm.elm
+++ src/Relm.elm
@@ -35,6 +35,13 @@ type alias State =
     }
 
 
+tileHeight = 32
+tileWidth = 32
+
+gameHeight = 400
+gameWidth = 480
+
+
 initialState = 
     { scene = Intro
     , character = 
@@ -48,16 +55,43 @@ initialState =
         }
     }
 
-view state = 
-    show ("Awesome stats = vx: " ++ (toString state.character.vx) ++ " vy: " ++ (toString state.character.vy)) 
+
+gameStats state =
+    "vx: " ++ (toString state.character.vx) ++ " vy: " ++ (toString state.character.vy) 
+    |> Text.fromString
+    |> leftAligned
+
+
+gameContainer =
+    container gameWidth gameHeight middle
+    
+mainCharacter : Character -> Form
+mainCharacter character =
+    rect 20 20 
+    |> filled red
+    |> move (toFloat character.position.x, toFloat character.position.y)
+    
+view : State -> Element
+view state =
+    container gameWidth gameHeight middle <|
+    collage gameWidth gameHeight
+        [ toForm(gameStats state) |> move (50 - gameWidth / 2, 20 - gameHeight / 2)
+        , mainCharacter state.character
+        ]
 
 update : {x: Int, y: Int} -> State -> State
 update {x, y} state =
     let 
+        oldPosition = state.character.position
+        newPosition = { oldPosition |
+                        x = oldPosition.x + x,
+                        y = oldPosition.y + y 
+                      }
         oldCharacter = state.character
         newCharacter = { oldCharacter |
                             vx = x,
-                            vy = y
+                            vy = y,
+                            position = newPosition
                        }
     in
         { state | character = newCharacter }
@@ -68,8 +102,11 @@ main =
 
 input = 
     Keyboard.arrows
-    
+
+speed = 3
+
+clock : Signal {x: Int, y: Int}    
 clock =
-    Signal.map2 (\dt arrows -> arrows) (fps 30) input
+    Signal.map2 (\dt arrows -> { arrows | x = arrows.x * speed, y = arrows.y * speed }) (fps 30) input
     
     
