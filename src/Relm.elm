fbefdd911bc27189e637e0d013b4edfc57009001
David Ribba <dribba@gmail.com>
2015-12-25 15:40:56 -0500

    Render grass :D

--- src/Relm.elm
+++ src/Relm.elm
@@ -21,6 +21,11 @@ type alias Position =
     { x: Int
     , y: Int
     }
+    
+type alias Velocity =
+    { x: Int
+    , y: Int
+    }
 
 type alias Character =
     { position: Position 
@@ -35,11 +40,11 @@ type alias State =
     }
 
 
-tileHeight = 32
-tileWidth = 32
+tileHeight = 40
+tileWidth = 40
 
-gameHeight = 400
 gameWidth = 480
+gameHeight = 400
 
 initialState = 
     { scene = Intro
@@ -55,12 +60,21 @@ initialState =
     }
 
 
--- VIEW 
+-- VIEW
+groundSprites : String
+groundSprites = "img/ground.png"
+
+grass : Element
+grass = croppedImage (240, 0) tileWidth tileHeight groundSprites
+
+
+gameStats : State -> Element
 gameStats state =
     "vx: " ++ (toString state.character.vx) ++ " vy: " ++ (toString state.character.vy) 
     |> Text.fromString
     |> leftAligned
 
+gameContainer : Element -> Element
 gameContainer =
     container gameWidth gameHeight middle
     
@@ -73,25 +87,43 @@ mainCharacter character =
 printStats : Element -> Form
 printStats stats =
     toForm(stats) |> move (50 - gameWidth / 2, 20 - gameHeight / 2)
+
+gridSize = (12, 10)
+
+createAndPlace : Int -> Int -> Element -> Form
+createAndPlace x y tile =
+    let 
+        posX = ((toFloat x) * tileWidth + (tileWidth / 2) - (gameWidth / 2))
+        posY = ((toFloat y) * tileHeight  + (tileHeight / 2) - (gameHeight / 2))
+    in
+        toForm(tile) |> move (posX, posY)
+
+grid : (Int, Int) -> Element -> List Form
+grid (w, h) tile = List.concatMap (\ix -> List.map (\iy -> createAndPlace ix iy tile) [0..(h-1)])  [0..(w-1)] 
+
+stage : Element
+stage =  
+    collage gameWidth gameHeight (grid gridSize grass)
     
 view : State -> Element
 view state =
-    container gameWidth gameHeight middle <|
+    gameContainer <|
     collage gameWidth gameHeight
-        [ printStats (gameStats state)
+        [ toForm stage
+        , printStats (gameStats state)
         , mainCharacter state.character
         ]
 
 
 -- UPDATE
-updatePosition : {x: Int, y: Int} -> Position -> Position
+updatePosition : Velocity -> Position -> Position
 updatePosition {x, y} position =
     { position |
         x = position.x + x,
         y = position.y + y  
     }
 
-updateCharacter : {x: Int, y: Int} -> Character -> Character
+updateCharacter : Velocity -> Character -> Character
 updateCharacter ({x, y} as speed) character = 
     { character |
         vx = x,
@@ -99,21 +131,22 @@ updateCharacter ({x, y} as speed) character =
         position = updatePosition speed character.position
     }
 
-update : {x: Int, y: Int} -> State -> State
+update : Velocity -> State -> State
 update speed state =
     { state | character = updateCharacter speed state.character}
 
 
 -- MAIN
+main : Signal Element
 main =
     Signal.map view (Signal.foldp update initialState clock)
 
 
 -- KEYS
-input : Signal { x: Int, y: Int }
+input : Signal Velocity
 input = 
     Keyboard.arrows
 
-clock : Signal {x: Int, y: Int}    
+clock : Signal Velocity    
 clock =
     Signal.map2 (\dt arrows -> arrows) (fps 30) input
